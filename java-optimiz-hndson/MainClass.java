9c716401e39d59a5e222f0dcecfc7d81edb346ca
gokuloganathan <skillgallery75@gmail.com>
2025-01-24 07:19:44 +0530

    enum learned

---
+++ java-optimiz-hndson/MainClass.java
@@ -0,0 +1,7 @@
+public class MainClass{
+
+    public static void main(String[] args) {
+        var status = OrderStatus.NEW;
+        System.out.println(status);
+    }
+}
---
+++ java-optimiz-hndson/OrderStatus.java
@@ -0,0 +1,22 @@
+public enum OrderStatus {
+    NEW {
+        @Override
+        public void handle() {
+            System.out.println("Processing new order.");
+        }
+    },
+    SHIPPED {
+        @Override
+        public void handle() {
+            System.out.println("Order shipped.");
+        }
+    },
+    DELIVERED {
+        @Override
+        public void handle() {
+            System.out.println("Order delivered.");
+        }
+    };
+
+    public abstract void handle();
+}
