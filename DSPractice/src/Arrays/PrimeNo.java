2b053fa61e32d99c2de0fcc608f3e3ac6d307a49
gokuloganathan <skillgallery75@gmail.com>
2025-10-05 13:13:47 +0530

    Data structure Binery Search in lecture tryouts

---
+++ DSPractice/src/Arrays/PrimeNo.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class PrimeNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no :");
+        int num = sc.nextInt();
+
+        
+        for (int i = 0; i < num; i++) {
+            System.out.println();
+//            String
+        }
+    }
+}
---
+++ DSPractice/src/BS_Assignments/MySqrt.java
@@ -0,0 +1,7 @@
+package BS_Assignments;
+
+public class MySqrt {
+    public static void main(String[] args) {
+        
+    }
+}
